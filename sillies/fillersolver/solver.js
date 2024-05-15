88d52d6f9b66fecb115df38140aa3bfebe2a7842
ItsAsShrimpleAsThat <155571246+ItsAsShrimpleAsThat@users.noreply.github.com>
2024-05-15 00:55:16 -0700

    CANVAS AND JAVASCRIPT WOOOO

---
+++ sillies/fillersolver/solver.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById("colorcanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "rgb(200, 200, 200)";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
